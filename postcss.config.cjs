1869180a51efa6a61cd8dc7583a1d7eafc0becf7
Rus Miller <lancemonotone@gmail.com>
2023-12-30 10:28:41 -0500

    Add project code style settings and update dependencies
    This change introduces project-specific code style settings for different languages. It also modifies the README formatting and the astro.config file to add a new dependency - astro-icon. The code style settings ensure consistency in code formatting across the project. These were saved in ".idea" directory.

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require("autoprefixer"),
+    require("postcss-preset-env")({
+      stage   : 1,
+      browsers: ["last 2 versions", "not ie <= 8"],
+    }),
+  ],
+}
